78cbc674b76458c398f905d278dc4d5f0fd49119
Tristan F <LeoDog896@hotmail.com>
2023-05-16 19:58:11 -0400

    feat: jkogan generation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,9 @@
 	import { Canvas, InteractiveObject, OrbitControls, T } from '@threlte/core'
 	import { spring } from 'svelte/motion'
 	import { degToRad } from 'three/src/math/MathUtils'
+    import { generators, getMidPoint, add, neg } from '../lib/methods';
+
+    const firstGenerator = generators[0];
 
 	const scale = spring(1)
 </script>
@@ -9,34 +12,30 @@
 <div>
 	<Canvas>
 		<T.PerspectiveCamera makeDefault position={[10, 10, 10]} fov={24}>
-			<OrbitControls maxPolarAngle={degToRad(80)} enableZoom={false} target={{ y: 0.5 }} />
+			<OrbitControls enableDamping={true} enableZoom={true}/>
 		</T.PerspectiveCamera>
 
 		<T.DirectionalLight castShadow position={[3, 10, 10]} />
 		<T.DirectionalLight position={[-3, 10, -10]} intensity={0.2} />
 		<T.AmbientLight intensity={0.2} />
+        
+        {@const points = firstGenerator.gen(100)}
+        {@const midPoint = getMidPoint(points)}
 
-		<!-- Cube -->
-		<T.Group scale={$scale}>
-			<T.Mesh position.y={0.5} castShadow let:ref>
-				<!-- Add interaction -->
-				<InteractiveObject
-					object={ref}
-					interactive
-					on:pointerenter={() => ($scale = 2)}
-					on:pointerleave={() => ($scale = 1)}
-				/>
+        {#each firstGenerator.gen(100) as point})}
+            <T.Mesh scale={0.05} position={add(point, neg(midPoint))} castShadow let:ref>
+                <!-- Add interaction -->
+                <InteractiveObject
+                    object={ref}
+                    interactive
+                    on:pointerenter={() => ($scale = 2)}
+                    on:pointerleave={() => ($scale = 1)}
+                />
 
-				<T.BoxGeometry />
-				<T.MeshStandardMaterial color="#333333" />
-			</T.Mesh>
-		</T.Group>
-
-		<!-- Floor -->
-		<T.Mesh receiveShadow rotation.x={degToRad(-90)}>
-			<T.CircleGeometry args={[3, 72]} />
-			<T.MeshStandardMaterial color="white" />
-		</T.Mesh>
+                <T.SphereGeometry />
+                <T.MeshStandardMaterial color="#333333" />
+            </T.Mesh>
+        {/each}
 	</Canvas>
 </div>
 
