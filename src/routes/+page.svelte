6f403660c8ecd1d8671eb9760d26560faf31dd0d
Tristan F <LeoDog896@hotmail.com>
2023-05-16 20:39:02 -0400

    refactor: remove unused interactivity
    maybe another date!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <script>
 	import { Canvas, InteractiveObject, OrbitControls, T } from '@threlte/core';
-	import { spring } from 'svelte/motion';
-	import { degToRad } from 'three/src/math/MathUtils';
 	import { generators, getMidPoint, add, neg } from '../lib/methods';
 
 	const firstGenerator = generators[0];
 
-	const scale = spring(1);
+	const points = firstGenerator.gen(100)
+	const midPoint = getMidPoint(points)
+
 </script>
 
 <div>
@@ -19,17 +19,11 @@
 		<T.DirectionalLight position={[-3, 10, -10]} intensity={0.2} />
 		<T.AmbientLight intensity={0.2} />
 
-		{@const points = firstGenerator.gen(100)}
-		{@const midPoint = getMidPoint(points)}
-
-		{#each firstGenerator.gen(100) as point})}
-			<T.Mesh scale={0.05} position={add(point, neg(midPoint))} castShadow let:ref>
-				<!-- Add interaction -->
+		{#each points as point}
+			<T.Mesh position={add(point, neg(midPoint))} castShadow let:ref>
 				<InteractiveObject
 					object={ref}
 					interactive
-					on:pointerenter={() => ($scale = 2)}
-					on:pointerleave={() => ($scale = 1)}
 				/>
 
 				<T.SphereGeometry />
